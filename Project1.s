22fafe2118fcd52139a590851c3778a22160793c
henryodunze <henryodunze44@gmail.com>
2019-10-31 00:17:19 -0400

    Created a Decimal converter that will be performed with a jump and carry out convertion functions

--- Project1.s
+++ Project1.s
@@ -15,6 +15,16 @@
 
 						Loop:
 							 # Perform Loop through the string to use Counters , increments and jump to sub-string
-							 add $t5, $t1, $s0								# Increment counter to the address of the string
-							 lb $a2, 0($t5)										#retrieve string current value
-							 
+							 add $t0, $t1, $s0								# Increment counter to the address of the string
+							 lb $a2, 0($t0)										# retrieve string current value
+							 jal DeciConverter								# Perform jump to get current decimal value in subroutine
+
+
+
+						DeciConverter:
+						# Performs conversion using my Howard ID = 02826194 with formula N = 26 + (X % 11) where N = 34.
+						# With M = N - 10 calculation shows M = 24 considered to be my range, value = (A &'a' to X &'x')
+						# Range = (0 to 9) to equal (0 to 9) Range = (A &'a' to X &'x') to equal (10 to 33)
+
+							add $t6, $zero, $a2								# line duplicates values in reg $a2 to reg $t6
+							addi $t5, $zero, 87								# ASCII ref value of 87 loaded to temporary reg $t5
