c2585a142b11c026d92792488657b44447492890
henryodunze <henryodunze44@gmail.com>
2019-10-30 21:30:59 -0400

    Performed loop through strings, implemented counters and checked string end

--- Project1.s
+++ Project1.s
@@ -5,13 +5,16 @@
 	main:
             li $v0, 8                           # instruction get input from user
             la $a0, user_input									# This instruction load register of input
-            li $a1, 10													# This read largest characters of input
+            li $a1, 11													# This read largest characters of input
             syscall
 
 						la $s0, user_input                  # Gets the address of the stored input string
 						addi $t1, $zero, 0                  # Initialize the offsets to 0
+						addi $t4, $zero, 10									# Checked for string ending with reg $t4
+						addi $t3, $zero, 0									# Sum given to 0 is held by register $t3
 
-						Loop_1:
-									add $t0, $t1, $s0               # Increment the address of the string and store
-									lb $a2, 0($t0)                  # Get the current character
-									jal ConvertToDecimal             
+						Loop:
+							 # Perform Loop through the string to use Counters , increments and jump to sub-string
+							 add $t5, $t1, $s0								# Increment counter to the address of the string
+							 lb $a2, 0($t5)										#retrieve string current value
+							 
